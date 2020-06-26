7e59a606b882e6e7f49701bbbd25ed87623a8f6c
Priyanshu Saxena Local <y.priyanshusaxena@gmail.com>
2020-06-26 10:36:50 +0530

    Features added: 	Useless Func 	uselessFile.c

--- SourceCode/Hello.c
+++ SourceCode/Hello.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
+void uselessFunc(){
+
+}
 void addFuncRemotely(){
 	printf("Remote Changes Done");
 }
---
+++ SourceCode/uselessFile.c
@@ -0,0 +1,3 @@
+djchdjldkkdjkdjidjeekdnjdhjnj
+vjfhfjcjjccdg
+priyanshusaxena
